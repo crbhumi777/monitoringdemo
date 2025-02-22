5262b3f239dc7dba615ec18c8e0f79a42ad30fdc
crbhumi777 <bhumikacr777@gmail.com>
2025-02-22 13:29:27 +0530

    monitoring using grafana and prometheus

---
+++ src/test/java/com/practice/monitoringdemo/MonitoringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.monitoringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
